52ba78305f65abab14f3b96749c7c7be695dd145
zienamarkos <91602875+zienamarkos@users.noreply.github.com>
2022-03-24 23:29:49 +0300

    updated

--- js/home.js
+++ js/home.js
@@ -4,3 +4,13 @@
      navbarLinks.classList.toggle('active')
 
  } ) 
+
+ const currentLocation=location.href;
+ const menuItem=document.querySelectorAll('a');
+ const menuLength=menuItem.length;
+
+ for(let i=0; i<menuLength; i++){
+     if(menuItem[i].href===currentLocation){
+         menuItem[i].className="active";
+     }
+ }
